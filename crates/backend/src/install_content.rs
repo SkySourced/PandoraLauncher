b81479d952234f1778d211d3b2cbde4b057496c2
Moulberry <james.jenour@protonmail.com>
2025-12-31 21:13:56 +0800

    Predownload modpack mods when installing modpack

--- crates/backend/src/install_content.rs
+++ crates/backend/src/install_content.rs
@@ -1,7 +1,7 @@
 use std::{ffi::OsString, io::Write, path::{Path, PathBuf}, sync::{atomic::AtomicBool, Arc}};
 
 use bridge::{
-    install::{ContentDownload, ContentInstall, ContentInstallFile}, instance::ModSummary, message::MessageToFrontend, modal_action::{ModalAction, ProgressTracker, ProgressTrackerFinishType}
+    install::{ContentDownload, ContentInstall, ContentInstallFile}, instance::{LoaderSpecificModSummary, ModSummary}, message::MessageToFrontend, modal_action::{ModalAction, ProgressTracker, ProgressTrackerFinishType}
 };
 use reqwest::StatusCode;
 use schema::content::ContentSource;
@@ -55,105 +55,16 @@ impl BackendState {
             tasks.push(async {
                 match content_file.download {
                     bridge::install::ContentDownload::Url { ref url, ref sha1, size } => {
-                        let _permit = semaphore.acquire().await.unwrap();
+                        let (path, hash, mod_summary) = self.download_file_into_library(&modal_action,
+                            &content_file.path, url, sha1, size, &semaphore).await?;
 
-                        let mut expected_hash = [0u8; 20];
-                        let Ok(_) = hex::decode_to_slice(&**sha1, &mut expected_hash) else {
-                            eprintln!("Content install has invalid sha1: {}", sha1);
-                            return Err(ContentInstallError::InvalidHash(sha1.clone()));
-                        };
-
-                        // Re-encode as hex just in case the given sha1 was uppercase
-                        let hash_as_str = hex::encode(expected_hash);
-
-                        let hash_folder = self.directories.content_library_dir.join(&hash_as_str[..2]);
-                        let _ = tokio::fs::create_dir_all(&hash_folder).await;
-                        let mut path = hash_folder.join(hash_as_str);
-
-                        if let Some(extension) = content_file.path.extension() {
-                            path.set_extension(extension);
-                        }
-
-                        let title = format!("Downloading {}", content_file.path.file_name().unwrap().to_string_lossy());
-                        let tracker = ProgressTracker::new(title.into(), self.send.clone());
-                        modal_action.trackers.push(tracker.clone());
-
-                        tracker.set_total(size);
-                        tracker.notify();
-
-                        let valid_hash_on_disk = {
-                            let path = path.clone();
-                            tokio::task::spawn_blocking(move || {
-                                crate::check_sha1_hash(&path, expected_hash).unwrap_or(false)
-                            }).await.unwrap()
-                        };
-
-                        if valid_hash_on_disk {
-                            tracker.set_count(size);
-                            tracker.set_finished(ProgressTrackerFinishType::Fast);
-                            tracker.notify();
-                            return Ok(InstallFromContentLibrary {
-                                mod_summary: self.mod_metadata_manager.get_path(&path),
-                                from: path,
-                                replace: content_file.replace_old.clone(),
-                                hash: expected_hash,
-                                content_file: content_file.clone(),
-                            });
-                        }
-
-                        let response = self.redirecting_http_client.get(&**url).send().await?;
-
-                        if response.status() != StatusCode::OK {
-                            return Err(ContentInstallError::NotOK(response.status()));
-                        }
-
-                        let mut file = tokio::fs::File::create(&path).await?;
-
-                        use futures::StreamExt;
-                        let mut stream = response.bytes_stream();
-
-                        let mut total_bytes = 0;
-
-                        let mut hasher = Sha1::new();
-                        while let Some(item) = stream.next().await {
-                            let item = item?;
-
-                            total_bytes += item.len();
-                            tracker.add_count(item.len());
-                            tracker.notify();
-
-                            hasher.write_all(&item)?;
-                            file.write_all(&item).await?;
-                        }
-
-                        tracker.set_finished(ProgressTrackerFinishType::Fast);
-
-                        let actual_hash = hasher.finalize();
-
-                        let wrong_hash = *actual_hash != expected_hash;
-                        let wrong_size = total_bytes != size;
-
-                        if wrong_hash || wrong_size {
-                            let _ = file.set_len(0).await;
-                            drop(file);
-                            let _ = tokio::fs::remove_file(&path).await;
-
-                            if wrong_hash {
-                                return Err(ContentInstallError::WrongHash);
-                            } else if wrong_size {
-                                return Err(ContentInstallError::WrongFilesize);
-                            } else {
-                                unreachable!();
-                            }
-                        }
-
-                        Ok(InstallFromContentLibrary {
-                            mod_summary: self.mod_metadata_manager.get_path(&path),
+                        return Ok(InstallFromContentLibrary {
                             from: path,
                             replace: content_file.replace_old.clone(),
-                            hash: expected_hash,
+                            hash,
                             content_file: content_file.clone(),
-                        })
+                            mod_summary
+                        });
                     },
                     bridge::install::ContentDownload::File { path: ref copy_path } => {
                         let title = format!("Copying {}", copy_path.file_name().unwrap().to_string_lossy());
@@ -264,5 +175,123 @@ impl BackendState {
                 modal_action.set_error_message(Arc::from(format!("{}", error).as_str()));
             },
         }
+    }
+
+    async fn download_file_into_library(&self, modal_action: &ModalAction, content_path: &Arc<Path>, url: &Arc<str>, sha1: &Arc<str>, size: usize, semaphore: &tokio::sync::Semaphore) -> Result<(PathBuf, [u8; 20], Option<Arc<ModSummary>>), ContentInstallError> {
+        let mut result = self.download_file_into_library_inner(modal_action, content_path.clone(), url, sha1, size, semaphore).await?;
+
+        if let Some(summary) = &result.2 {
+            if let LoaderSpecificModSummary::ModrinthModpack { downloads, .. } = &summary.extra {
+                let mut tasks = Vec::new();
+
+                for download in downloads.iter() {
+                    let path: PathBuf = typed_path::Utf8UnixPath::new(&*download.path).with_platform_encoding().into();
+
+                    tasks.push(self.download_file_into_library_inner(modal_action, path.into(),
+                        &download.downloads[0], &download.hashes.sha1, download.file_size, semaphore));
+                }
+
+                _ = futures::future::try_join_all(tasks).await;
+            }
+            result.2 = self.mod_metadata_manager.get_path(&result.0);
+        }
+
+        Ok(result)
+    }
+
+    async fn download_file_into_library_inner(&self, modal_action: &ModalAction, content_path: Arc<Path>, url: &Arc<str>, sha1: &Arc<str>, size: usize, semaphore: &tokio::sync::Semaphore) -> Result<(PathBuf, [u8; 20], Option<Arc<ModSummary>>), ContentInstallError> {
+        let _permit = semaphore.acquire().await.unwrap();
+
+        let mut expected_hash = [0u8; 20];
+        let Ok(_) = hex::decode_to_slice(&**sha1, &mut expected_hash) else {
+            eprintln!("Content install has invalid sha1: {}", sha1);
+            return Err(ContentInstallError::InvalidHash(sha1.clone()));
+        };
+
+        // Re-encode as hex just in case the given sha1 was uppercase
+        let hash_as_str = hex::encode(expected_hash);
+
+        let hash_folder = self.directories.content_library_dir.join(&hash_as_str[..2]);
+        let _ = tokio::fs::create_dir_all(&hash_folder).await;
+        let mut path = hash_folder.join(hash_as_str);
+
+        if let Some(extension) = content_path.extension() {
+            path.set_extension(extension);
+        }
+
+        let title = format!("Downloading {}", content_path.file_name().unwrap().to_string_lossy());
+        let tracker = ProgressTracker::new(title.into(), self.send.clone());
+        modal_action.trackers.push(tracker.clone());
+
+        tracker.set_total(size);
+        tracker.notify();
+
+        let valid_hash_on_disk = {
+            let path = path.clone();
+            tokio::task::spawn_blocking(move || {
+                crate::check_sha1_hash(&path, expected_hash).unwrap_or(false)
+            }).await.unwrap()
+        };
+
+        if valid_hash_on_disk {
+            tracker.set_count(size);
+            tracker.set_finished(ProgressTrackerFinishType::Fast);
+            tracker.notify();
+            let summary = self.mod_metadata_manager.get_path(&path);
+            return Ok((path, expected_hash, summary));
+        }
+
+        let response = self.redirecting_http_client.get(&**url).send().await?;
+
+        if response.status() != StatusCode::OK {
+            return Err(ContentInstallError::NotOK(response.status()));
+        }
+
+        // Tokio doesn't have lock, so we use std temporarily to lock it
+        let file = std::fs::File::create(&path)?;
+        _ = file.lock();
+
+        let mut file = tokio::fs::File::from_std(file);
+
+        use futures::StreamExt;
+        let mut stream = response.bytes_stream();
+
+        let mut total_bytes = 0;
+
+        let mut hasher = Sha1::new();
+        while let Some(item) = stream.next().await {
+            let item = item?;
+
+            total_bytes += item.len();
+            tracker.add_count(item.len());
+            tracker.notify();
+
+            hasher.write_all(&item)?;
+            file.write_all(&item).await?;
+        }
+
+        tracker.set_finished(ProgressTrackerFinishType::Fast);
+
+        let actual_hash = hasher.finalize();
+
+        let wrong_hash = *actual_hash != expected_hash;
+        let wrong_size = total_bytes != size;
+
+        if wrong_hash || wrong_size {
+            let _ = file.set_len(0).await;
+            drop(file);
+            let _ = tokio::fs::remove_file(&path).await;
+
+            if wrong_hash {
+                return Err(ContentInstallError::WrongHash);
+            } else if wrong_size {
+                return Err(ContentInstallError::WrongFilesize);
+            } else {
+                unreachable!();
+            }
+        }
+
+        let summary = self.mod_metadata_manager.get_path(&path);
+        Ok((path, expected_hash, summary))
     }
 }
